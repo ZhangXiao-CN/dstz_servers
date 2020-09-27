5320523ebeb397f668adde29ff3e3539a322c2bb
ZhangXiao <469338652@qq.com>
2020-09-28 00:28:02 +0800

    调整按作者查询文章的逻辑

--- routes/actions/post/findAuthor.js
+++ routes/actions/post/findAuthor.js
@@ -4,7 +4,8 @@ module.exports = async (req, res) => {
   const id = req.params.id
   // 查询用户信息
   const limit = req.query.limit ? parseInt(req.query.limit) : 5
-  const posts = await Post.find({ author: id })
+  const state = req.query.state ? parseInt(req.query.state) : 1
+  const posts = await Post.find({ author: id, state: state })
     .select('title meta summary thumbnail categoryChilren category updateAt createAt tag')
     .limit(limit)
   res.send(posts)
